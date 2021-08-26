fe0c1935ca4c995b25e23beae881cac1fc762bb4
LudOk1502 <lyudmyla152@gmail.com>
2021-08-26 21:16:46 +0300

    classwork lesson7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    if (action.obj === '1' && action.diya === '+') {
+        return {...state, a: state.a++}
+    } else if (action.obj === '1' && action.diya === '-') {
+        return {...state, a: state.a--}
+    } else if (action.obj === '2' && action.diya === '+') {
+        return {...state, b: state.b++}
+    } else if (action.obj === '2' && action.diya === '-') {
+        return {...state, b: state.b--}
+    } else if (action.obj === '3' && action.diya === '+') {
+        return {...state, c: state.c++}
+    } else if (action.obj === '3' && action.diya === '-') {
+        return {...state, c: state.c--}
+    }
+    return {...state};
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0})
+    return (
+        <div className="App">
+            <h1>state1 - {a}</h1>
+            <button onClick={() => dispatch({obj: '1', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '1', diya: '-'})}>dec</button>
+            <h1>state2 - {b}</h1>
+            <button onClick={() => dispatch({obj: '2', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '2', diya: '-'})}>dec</button>
+            <h1>state3 - {c}</h1>
+            <button onClick={() => dispatch({obj: '3', diya: '+'})}>inc</button>
+            <button onClick={() => dispatch({obj: '3', diya: '-'})}>dec</button>
+        </div>
+    );
 }
 
 export default App;
